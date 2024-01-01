08773bbb6e965c9bfe73bbc7fef3c109be7591ae
arnoN7 <arnaud.rover@gmail.com>
2024-01-01 23:56:13 +0200

    add multiple colors for tables

---
+++ er_diagram.py
@@ -0,0 +1,189 @@
+import csv
+import pandas as pd
+import graphviz
+from jinja2 import Environment, FileSystemLoader
+from enum import Enum
+import yaml
+import os
+
+
+# Generate PDM from the an xls file or a csv file
+
+class ERType(Enum):
+    PHYSICAL = 1
+    LOGICAL = 2
+    CONCEPTUAL = 3
+
+class Column:
+    def __init__(self, table_name:str, name:str, type:str, nullable:bool=True):
+        self.name = name
+        self.type = type
+        self.nullable = nullable
+        self.table_name = table_name
+        self.fk = None
+            
+    def __str__(self):
+        return f'{self.name} {self.type} nullable:{self.nullable}'
+
+class ColumnArg(Column):
+    pass
+
+class ColumnFK(Column):
+    mapping_cardinality = {
+        '0': 'none',
+        '1': 'tee',
+        'n': 'crow'
+    }
+    class FKErr(Exception):
+        pass
+    def __init__(self, table_name:str, name:str, type:str, fk:str, nullable:bool=True):
+        super().__init__(table_name, name, type, nullable)
+        self.columnfk = self.columnpk = name
+        self.table_pk = None
+        cardinality = None
+        self.cardinality = '0'
+        self.cardinalityFK = '0'
+        error_msg = f'ERROR: incorrect FK format "{fk}" in table {table_name} must be FK <fk_table>.<column> <cardinality>:<cardinality_fk>'
+        try:
+            fk_key, self.table_pk, cardinality = str(fk).split(' ')
+            if '.' in self.table_pk:
+                self.table_pk, self.columnpk = self.table_pk.split('.')
+            if ':' in cardinality:
+                self.cardinality = cardinality.split(':')[0]
+                self.cardinalityFK = cardinality.split(':')[1]
+            else:
+                raise ColumnFK.FKErr
+            if not fk_key == 'FK':
+                print(error_msg)
+                raise ColumnFK.FKErr
+        except:
+            print(error_msg)
+            raise ColumnFK.FKErr 
+    def getarrowhead(self):
+        try:
+            return ColumnFK.mapping_cardinality[self.cardinalityFK]
+        except:
+            print(f'ERROR: incorrect cardinality {self.cardinalityFK} must be 0, 1 or n')
+            raise ColumnFK.FKErr
+    def getarrowtail(self):
+        try:
+            return ColumnFK.mapping_cardinality[self.cardinality]
+        except:
+            print(f'ERROR: incorrect cardinality {self.cardinality} must be 0, 1 or n')
+            raise ColumnFK.FKErr
+
+class ColumnPK(Column):
+    class PKErr(Exception):
+        pass
+    def __init__(self, table_name:str, name:str, type:str, nullable:bool=False):
+        if nullable:
+            print(f'ERROR: PK column {name} is nullable')
+            raise ColumnPK.PKErr
+        super().__init__(table_name, name, type, nullable)
+        self.pk = True
+
+
+
+class Table:
+    def __init__(self, name:str, columnsPK:list[ColumnPK], columnsFK:list[ColumnFK], columnsArgs:list[ColumnArg], type:str='table'):
+        self.name = name
+        self.columnsPK = columnsPK
+        self.columnsFK = columnsFK
+        self.columnsArgs = columnsArgs
+        self.type = type
+    def __str__(self):
+        columns = '\n'.join([f'\t{column}' for column in self.columns])
+        return f'{self.name} {{\n{columns}\n}}'
+
+class ERDiagram:
+    def __init__(self, colors:dict):
+        self.tables = []
+        self.colors = colors
+    
+    def add_table(self, table:Table):
+        self.tables.append(table)
+
+    def __str__(self):
+        tables = '\n'.join([f'{table}' for table in self.tables])
+        return f'ERDiagram {{\n{tables}\n}}'
+    
+    def import_from_xlsx(self, file:str):
+        def get_table_type(sheet_name:str) -> tuple[str, str]:
+            for table_type in self.colors['TABLE_COLOR'].keys():
+                if sheet_name.lower().startswith(f"{table_type.lower()} "):
+                    return table_type, sheet_name.split(f'{table_type.lower()} ')[-1]
+            return None, None
+
+        try:
+            xls = pd.ExcelFile(file)
+            for table_sheet in xls.sheet_names:
+                table_type, table_name = get_table_type(table_sheet)
+                if not table_type:
+                    continue
+                df = pd.read_excel(xls, table_sheet)
+                column_list_pk = df.loc[df['pk'].notnull(), ['name', 'type', 'nullable']].to_dict('records')
+                columns_pk = [ColumnPK(table_name, **column) for column in column_list_pk]
+                column_list_fk = df.loc[df['fk'].astype(str).str.startswith("FK "), ['name', 'type', 'fk', 'nullable']].to_dict('records')
+                columns_fk = [ColumnFK(table_name, **column) for column in column_list_fk] 
+                columns_args = df.loc[df['pk'].isnull() & df['fk'].isnull(), ['name', 'type', 'nullable']].to_dict('records')
+                columns_args = [ColumnArg(table_name, **column) for column in columns_args]
+
+                table = Table(table_name, columns_pk, columns_fk, columns_args, type=table_type.upper())
+                self.add_table(table)
+        except (ColumnPK.PKErr, ColumnFK.FKErr) as e:
+            return ('ERROR', e)
+            pass
+            
+    
+    def draw_pdf(self, file:str, er_type:ERType=ERType.PHYSICAL):
+        model_graph = graphviz.Digraph('Database', filename=file, graph_attr={'concentrate': 'true', 'rankdir': 'LR'})
+        for table in self.tables:
+            table_struct = get_html_table(table, self.colors, er_type)
+            if er_type == ERType.CONCEPTUAL:
+                model_graph.node(table.name, fillcolor=self.colors['TABLE_COLOR'][table.type]['BG_COLOR'], style='filled', shape=get_shape(er_type), fontcolor=self.colors['TABLE_COLOR'][table.type]['FONT_COLOR'])
+            else:
+                model_graph.node(table.name, label=table_struct, shape=get_shape(er_type))
+
+        for table in self.tables:
+            for fk in table.columnsFK:
+                model_graph.edge(f'{table.name}:{fk.name}', f'{fk.table_pk}:{fk.columnpk}',arrowhead=fk.getarrowhead(), 
+                                arrowtail=fk.getarrowtail(), dir='both', color=self.colors['ARROW_COLOR'])
+        model_graph.render(outfile=file, filename=file.replace('.pdf', '.gv'), view=True)
+        return ('OK', f'{file}.pdf')
+        
+
+
+
+def read_csv(file_path:str):
+    with open(file_path, 'r') as f:
+        reader = csv.reader(f)
+        return list(reader)
+
+def get_html_table(table:Table, colors:dict, er_type:ERType=ERType.PHYSICAL):
+    if er_type == ERType.CONCEPTUAL:
+        return table.name
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    template = environment.get_template("html_table_template.html")
+    content = template.render(table=table, er_type=er_type.value, colors=colors['TABLE_COLOR'], ERType=ERType)
+    if not os.path.exists('debug'):
+        os.makedirs('debug')
+    with open(f"debug/{table.name}.html", mode="w", encoding="utf-8") as message:
+        message.write(content)
+    return content
+
+def get_shape(er_type:ERType):
+    if er_type == ERType.PHYSICAL or er_type == ERType.LOGICAL:
+        return 'none'
+    elif er_type == ERType.CONCEPTUAL:
+        return 'box'
+    else:
+        print(f'ERROR: incorrect ER type {er_type}')
+        raise ValueError(f'incorrect ER type {er_type}')
+
+if __name__ == '__main__':
+    #read colors from yaml file
+    with open('config_colors.yml') as f:
+        colors = yaml.load(f, Loader=yaml.FullLoader)
+        er = ERDiagram(colors)
+        er.import_from_xlsx('example/sample_datamodel.xlsx')
+        er.draw_pdf('example/sample_datamodel.pdf', er_type=ERType.PHYSICAL)
